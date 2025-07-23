9d6c0fb0de1e0a86eaea128e3841514770816188
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-07-23 17:02:53 +0700

    Criteria Query

--- src/test/java/com/elastic/elastic_spring/NativeAndCriteriaQueryTest.java
+++ src/test/java/com/elastic/elastic_spring/NativeAndCriteriaQueryTest.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 
 import com.elastic.elastic_spring.entity.Garment;
 import com.elastic.elastic_spring.entity.Product;
@@ -15,6 +19,9 @@ public class NativeAndCriteriaQueryTest extends AbstractTest {
 	@Autowired
 	private GarmentRepository repository;
 
+	@Autowired
+	private ElasticsearchOperations elasticsearchOperations;
+
 	@BeforeAll
 	public void dataSetup() {
 		var garments = this.readResource("garments.json", new TypeReference<List<Garment>>() {
@@ -22,5 +29,29 @@ public class NativeAndCriteriaQueryTest extends AbstractTest {
 		});
 		this.repository.saveAll(garments);
 		Assertions.assertEquals(20, this.repository.count());
+	}
+
+	@Test
+	public void criteriaQuery() {
+		var nameIsShirt = Criteria.where("name").is("shirt");
+		verify(nameIsShirt, 1);
+		var priceAbove100 = Criteria.where("price").greaterThan(100);
+		verify(priceAbove100, 5);
+
+		verify(nameIsShirt.or(priceAbove100), 6);
+
+		var brandIsZara = Criteria.where("brand").is("Zara");
+		verify(priceAbove100.and(brandIsZara.not()), 3);
+
+		var fuzzyMatchShort = Criteria.where("name").fuzzy("short");
+		verify(fuzzyMatchShort, 1);
+	}
+
+	private void verify(Criteria criteria, int expectedResultsCount) {
+		var query = CriteriaQuery.builder(criteria).build();
+		var searchHits = this.elasticsearchOperations.search(query, Garment.class);
+		searchHits.forEach(this.print());
+		Assertions.assertEquals(expectedResultsCount, searchHits.getTotalHits());
+
 	}
 }
